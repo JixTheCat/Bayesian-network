598a18da49f8a775838a16fadfb21e2c489e5c4c
JixTheCat <bryce.polley@gmail.com>
2023-10-24 09:14:30 +1000

    added shell sscript to build for qol

---
+++ bn/build.sh
@@ -0,0 +1,7 @@
+mkdir build
+cd build
+
+cmake -DCMAKE_PREFIX_PATH=/usr/include/c++/11/libtorch ..
+cmake --build . --config Release
+
+cmake -DCMAKE_PREFIX_PATH=`python3 -c 'import torch;print(torch.utils.cmake_prefix_path)'` ..
